1cf1d1532a9beb801178d75c57409e324aa0ca97
KELSEY JUNIOR <kelseytfjunior@gmail.com>
2020-12-12 10:30:32 -0300

    fix: Block duplicated movies

--- src/__tests__/unit/CreateMovieService.spec.ts
+++ src/__tests__/unit/CreateMovieService.spec.ts
@@ -36,6 +36,42 @@ describe('Save Movie - Unity', () => {
     expect(movie.movieId).toBe(movieId);
   });
 
+  it('should not be able to save movies when its already exists on application', async () => {
+    const mockMovie1 = movieDetails;
+    const movie1 = moviesRepository.create({
+      adult: mockMovie1.adult,
+      backdropPath: mockMovie1.backdrop_path,
+      belongsToCollection: mockMovie1.belongs_to_collection,
+      budget: mockMovie1.budget,
+      genres: mockMovie1.genres,
+      homepage: mockMovie1.homepage,
+      imdbId: mockMovie1.imdb_id,
+      movieId: mockMovie1.id,
+      originalLanguage: mockMovie1.original_language,
+      originalTitle: mockMovie1.original_title,
+      overview: mockMovie1.overview,
+      popularity: mockMovie1.popularity,
+      posterPath: mockMovie1.poster_path,
+      productionCompanies: mockMovie1.production_companies,
+      productionCountries: mockMovie1.production_countries,
+      releaseDate: mockMovie1.release_date,
+      revenue: mockMovie1.revenue,
+      runtime: mockMovie1.runtime,
+      spokenLanguage: mockMovie1.spoken_languages,
+      status: mockMovie1.status,
+      tagline: mockMovie1.tagline,
+      title: mockMovie1.title,
+      video: mockMovie1.video,
+      voteAverage: mockMovie1.vote_average,
+      voteCount: mockMovie1.vote_count,
+    });
+    await moviesRepository.save(movie1);
+
+    const createMovie = new CreateMovieService();
+
+    await expect(createMovie.execute({ movieId: mockMovie1.id })).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should not be able to save movies when resource could not be found', async () => {
     const movieId = 550;
 
